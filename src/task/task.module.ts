ee24dcbc22df66021a3fe15ea76312712433f50a
codeinvisible <lamasandesh400@gmail.com>
2023-01-18 17:05:48 +0545

    removed comments

--- src/task/task.module.ts
+++ src/task/task.module.ts
@@ -1,11 +1,9 @@
-import { Module } from "@nestjs/common";
-import { AuthModule } from "src/auth/auth.module";
-import { TypeOrmExModule } from "src/database/typeorm-ex.module";
-import { TaskController } from "./task.controller";
-import { TaskRepository } from "./task.repository";
-import { TaskService } from "./task.service";
-
-// This change is made on git
+import { Module } from '@nestjs/common';
+import { AuthModule } from 'src/auth/auth.module';
+import { TypeOrmExModule } from 'src/database/typeorm-ex.module';
+import { TaskController } from './task.controller';
+import { TaskRepository } from './task.repository';
+import { TaskService } from './task.service';
 
 @Module({
   imports: [TypeOrmExModule.forCustomRepository([TaskRepository]), AuthModule],
